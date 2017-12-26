d41a3f78c9ad69664c9ca0bfb87c74bebc37d2d6
brute4s99 <piyushaggarwal002@gmail.com>
2017-12-26 12:23:50 +0530

    Reset x.cpp

--- cpp/x.cpp
+++ cpp/x.cpp
@@ -8,17 +8,6 @@ int main() {
 	while (t--) {
 		int n;
 		cin >> n;
-		vector<int> one,two,three;
-		int t;
-		for(int i=0;i<n;i++){
-		cin>>t;
-		if(t%4==1) one.push_back(t);
-		else if(t%4==2) two.push_back(t);
-		else if(t%4==3) three.push_back(t);
-		}
-		bool flag=1; // optimistic !
-		if(two.size())
-
 
 		cout<<'\n';
 
