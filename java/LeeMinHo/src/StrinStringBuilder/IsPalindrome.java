0acb23311e5e820cc69ee074a5c5a414871bf74f
brute4s99 <piyushaggarwal002@gmail.com>
2018-01-14 18:42:29 +0530

    Uploaded via GitBot v0.5

---
+++ java/LeeMinHo/src/StrinStringBuilder/IsPalindrome.java
@@ -0,0 +1,21 @@
+package StrinStringBuilder;
+
+public class IsPalindrome {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static boolean ispalindrome(String str){
+		for(int i=0, j=str.length()-1; i<=j; i++, j--){
+			if(str.charAt(i)!=str.charAt(j)){
+				
+			}
+		}
+	}
+
+}
