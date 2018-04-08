ca9b34e2d2762565de10c9e055933ce97e062ab0
brute4s99 <piyushaggarwal002@gmail.com>
2018-04-08 12:42:30 +0530

    Coding BLocks APAT

--- cpp/x.cpp
+++ cpp/x.cpp
@@ -8,28 +8,13 @@ int main() {
 	int t;
 	cin >> t;
 	while (t--) {
-<<<<<<< HEAD
-=======
-		long long tnl;
-		cin>>tnl;
-		long long tnls[tnl],c,d,v,tnl_time;
-		for(long long i=0;i<tnl;i++) cin>>tnls[i];
-		cin>>c>>d>>v;
-		tnl_time=d/v;
-		long long delay=tnls[0];
-		long long i=1;
-		while(i<tnl){
-			if(tnls[i]-tnls[i-1]>0){
-				cout<<"from tunnel "<<i-1<<"to tunnel "<<i<<" delay of "<<tnls[i]-tnls[i-1]<<'\n';
-				delay+=tnls[i]-tnls[i-1];
-			}
-			i++;
-
-		}
-		cout<<delay*(c-1);
-		cout << ".000000000\n";
->>>>>>> 9d8c151222d12c3effc17e3cad94e7c9d238db66
-
+		int m,n;
+		cin>>m>>n;
+		int Aayush=0,Harshit=0;
+		Aayush=sqrt(m);
+		Harshit=(-1+sqrt(1+4*n))/2;
+		if(Harshit>=Aayush)	cout<<"Harshit";
+		else cout << "Aayush";
 		cout << "\n";
 	}
 }
