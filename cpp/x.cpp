681f18bc52dcc6d17e1e640e18c01277f0827cc4
brute4s99 <piyushaggarwal002@gmail.com>
2018-05-01 17:37:02 +0530

    code_gladiators

--- cpp/x.cpp
+++ cpp/x.cpp
@@ -1,26 +1,47 @@
+#include <algorithm>
 #include <cmath>
 #include <iostream>
-#include <algorithm>
-#include <vector>
 #include <string>
-#define print(x) cout<<#x<<" = "<<x<<'\n';
+#include <vector>
+
+#define print(x) //cout << #x << " = " << x << '\n';
 using namespace std;
-<<<<<<< HEAD
-int main()
-{
-	int t;
-	cin >> t;
-	while (t--)
-	{
-		cout << "Hello, World!!!!";
-		cout << "\n";
-	}
-=======
-int xx;
-int main(){
-	int p;
-	cin >> p;
-	int parent[p];
-	for (int i = 0; i <)
->>>>>>> f32b04d8a10e36c881ea7f35861902202ebc70a8
+void rem_node(int parent[100], int p, int r);
+int main() {
+  int p, i, flag = 0;
+  cin >> p;
+  int parent[p], leaf = 0;
+  for (i = 0; i < p; i++) {
+    cin >> parent[i];
+  }
+  for (i = 0; i < p; i++)
+    //cout << " Parent of " << i << " -> " << parent[i] << '\n';
+  int r;
+  cin >> r;
+  rem_node(parent, p, r);
+  for (i = 0; i < p; i++)
+    //cout << parent[i] << ' ';
+  //cout << '\n';
+  for (i = 0; i < p; i++) {
+      if (parent[i] != -2) {
+    flag = 0;
+    for (int j = 0; j < p; j++)
+        if (i == parent[j])
+          flag = 1;
+        if (flag == 0) {
+          //cout << "at i =" << i << '\n';
+          leaf++;
+        }
+      }
+  }
+  cout << leaf << '\n';
+}
+
+// every removed node will become -2
+void rem_node(int parent[100], int p, int r) {
+  parent[r] = -2;
+  for (int i = 0; i < p; i++) {
+    if (parent[i] == r)
+      rem_node(parent, p, i);
+  }
 }
