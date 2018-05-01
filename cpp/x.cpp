2cae2ad538a67a25b9931d2a8de1a6cc2fc2f515
brute4s99 <piyushaggarwal002@gmail.com>
2018-05-01 17:06:48 +0530

    yo

--- cpp/x.cpp
+++ cpp/x.cpp
@@ -1,35 +1,16 @@
-#include<iostream>
 #include <cmath>
-#include<algorithm>
-#include<vector>
+#include <iostream>
+#include <algorithm>
+#include <vector>
 #include <string>
 using namespace std;
-int main() {
+int main()
+{
 	int t;
 	cin >> t;
-	while (t--) {
-<<<<<<< HEAD
-=======
-		long long tnl;
-		cin>>tnl;
-		long long tnls[tnl],c,d,v,tnl_time;
-		for(long long i=0;i<tnl;i++) cin>>tnls[i];
-		cin>>c>>d>>v;
-		tnl_time=d/v;
-		long long delay=tnls[0];
-		long long i=1;
-		while(i<tnl){
-			if(tnls[i]-tnls[i-1]>0){
-				cout<<"from tunnel "<<i-1<<"to tunnel "<<i<<" delay of "<<tnls[i]-tnls[i-1]<<'\n';
-				delay+=tnls[i]-tnls[i-1];
-			}
-			i++;
-
-		}
-		cout<<delay*(c-1);
-		cout << ".000000000\n";
->>>>>>> 9d8c151222d12c3effc17e3cad94e7c9d238db66
-
+	while (t--)
+	{
+		cout << "Hello, World!!!!";
 		cout << "\n";
 	}
 }
