eb23ae93a77328b137f9026446b469aab350d4d8
Piyush (PC) <piyushaggarwal002@gmail.com>
2018-07-10 13:48:38 +0530

    removes extra spaces

--- cpp/TECHGIG.cpp
+++ cpp/TECHGIG.cpp
@@ -9,9 +9,9 @@ using namespace std;
 
 int main()
 {
-  string a;
-  getline(cin, a);
-  string str2 = "  " ;
-  int found = a.find(str2);
-  if (found != string::npos) cout << "first '  ' found at: " << found << '\n';
+    int t;
+    cin>>t;
+    while(t--){
+        
+    }
 }
